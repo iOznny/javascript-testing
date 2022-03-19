f0766097ce5aa516f79a1d00d6273784e489228a
iOznny <f000170014@gmail.com>
2022-03-18 19:57:04 -0600

    C

--- testing-js/js/01-app.js
+++ testing-js/js/01-app.js
@@ -4,11 +4,26 @@ function sum(a, b) {
     return a + b;
 }
 
-let result = sum(1, 2);
+function subtract(a, b) {
+    return a - b;
+}
+
+// Sum
+let result = sum(2, 2);
 let response = 3;
 
 if (result != response) {
     console.error('El resultado es diferente a lo esperado.');
 } else {
-    console.log('La prueba paso correctamente.');
+    console.log('La suma paso correctamente.');
+}
+
+// Subtract
+let result_subtract = subtract(10, 5);
+let response_subtract = 5;
+
+if (result_subtract != response_subtract) {
+    console.error('El resultado es diferente a lo esperado.');
+} else {
+    console.log('La resta paso correctamente.');
 }
