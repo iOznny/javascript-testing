af070b47b0019937bc02ef20f2cfa9ce818c7e0f
iOznny <f000170014@gmail.com>
2022-03-22 16:46:32 -0600

    Add snapshots

--- __tests__/functions.test.js
+++ __tests__/functions.test.js
@@ -1,5 +1,30 @@
 // Testing Functions
+function sum(a, b) {
+    return a + b;
+}
 
-describe('', () => {
+function subtract(a, b) {
+    return a - b;
+}
 
+describe('Testing Functions Sum & Subtract', () => {
+    // Sum
+    test('Sum 20 + 30', () => {
+        expect(sum(20, 30)).toBe(50);
+    });
+
+    // Subtract
+    test('Subtract 10 - 5', () => {
+        expect(subtract(10, 5)).toBe(5);
+    });
+
+    // Testing sum not is 10
+    test('Sum 10 + 10 not be the same ', () => {
+        expect(sum(10, 10)).not.toBe(10);
+    });
+
+    // Testing subtract not is 10
+    test('Subtract 10 - 5 not be the same', () => {
+        expect(subtract(10, 5)).not.toBe(2);
+    })
 });
